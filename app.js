b386727e96109255936823811acee1029dcdba5a
cameron <cameronshaw95@gmail.com>
2020-04-28 23:03:16 +1200

    port change

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// const io = require('socket.io')(app);
+const io = require('socket.io')(port);
 
 // app.get('/', (req, res) => res.send('Thomas Lennon Croskery'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+  
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
