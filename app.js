945c4171cec8835cb1faf5313a0151f9c8906ac8
cameron <cameronshaw95@gmail.com>
2020-04-29 17:00:35 +1200

    commenting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+// const http = require('http').createServer(app);
 // const port = 3000;
-const io = require('socket.io')(http);
+// const io = require('socket.io')(http);
 
 // app.get('/', (req, res) => res.send('Thomas Lennon Croskery'));
 
@@ -10,10 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
   
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-});
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         io.emit('chat message', msg);
+//     });
+// });
 
-app.listen(() => console.log(`Example app listening`));
+// app.listen(() => console.log(`Example app listening`));
