5380de2238e06ca585263176f4fe585e3c5f73e1
cameron <cameronshaw95@gmail.com>
2020-04-29 18:38:01 +1200

    delete npm

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const express = require('express');
-const app = express();
-// const http = require('http').createServer(app);
-const port = 3000;
-// const io = require('socket.io')(http);
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => res.send('Thomas Lennon Croskery'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-  
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-// });
-
-app.listen(() => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
