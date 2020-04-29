930468374276ecc03f1fe6fb495f69af284107e6
cameron <cameronshaw95@gmail.com>
2020-04-29 13:05:53 +1200

    bug http

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 // const port = 3000;
-const io = require('socket.io')(app);
+const io = require('socket.io')(http);
 
 // app.get('/', (req, res) => res.send('Thomas Lennon Croskery'));
 
